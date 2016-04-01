74d23db720de48a7f0ae442d736ea4aa115dda81
hexfet <hexfet@localhost>
2016-03-31 23:05:47 -0400

    Revert TX_EN change since d764a03 to see if 7e RX light flickering is affected.

--- src/protocol/frskyx_cc2500.c
+++ src/protocol/frskyx_cc2500.c
@@ -830,7 +830,7 @@ static void initialize(int bind)
     counter_rst = (chanskip - ctr) >> 2;
 
     frskyX_init(); 
-    CC2500_SetTxRxMode(TX_EN);  // enable PA 
+//    CC2500_SetTxRxMode(TX_EN);  // enable PA 
 
     if (bind) {
         PROTOCOL_SetBindState(0xFFFFFFFF);
